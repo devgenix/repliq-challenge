e7460a84e6b284859293205499f464f1d5b5f6db
Devgenix <emmaueln644@gmail.com>
2023-03-29 15:46:23 +0100

    Setup/configure root urls

--- Repliq/urls.py
+++ Repliq/urls.py
@@ -1,21 +1,40 @@
-"""Repliq URL Configuration
+# Swagger docs
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django imports
 from django.contrib import admin
 from django.urls import path
 
+# Own Imports
+from Repliq.schema_generator import CustomSchemaGenerator
+
+# Swagger docs
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Repliq Challenge API",
+        default_version="1.0",
+        description="Django app to track corporate assets such as phones, tablets, laptops and other gears handed out to employees",
+        contact=openapi.Contact(email="challenge@repliq.com"),
+    ),
+    public=True,
+    generator_class=CustomSchemaGenerator,
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('restriceted/admin/', admin.site.urls),
+
+    # Documentation by swagger
+    path(
+        "api/auth/docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="api_docs",
+    ),
+
+    # Documentation by redoc to be rendered on home page
+    path(
+        "",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="api_redocs",
+    ),
 ]

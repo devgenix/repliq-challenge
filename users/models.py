8e304ab7591d97930865de55c1f45dc4bc255a0a
Devgenix <emmaueln644@gmail.com>
2023-03-30 18:35:55 +0100

    update/defined cutom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,83 @@
+# Django imports
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
+from django.utils import timezone
+from django.core.mail import send_mail
 
-# Create your models here.
+# Local imports
+from Assets.models import Company, Device
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_company_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    # Company
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, null=True, blank=True)
+    devices = models.ManyToManyField(Device, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+    
+    def __str__(self) -> str:
+        return self.email + " " + self.first_name + " " + self.last_name
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """
+        Returns the short name for the user.
+        """
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'

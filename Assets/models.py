f24fe8ee1a728053635fbf3c95e40cc3508e4106
Devgenix <emmaueln644@gmail.com>
2023-04-03 11:40:15 +0100

    update/created asets models

--- Assets/models.py
+++ Assets/models.py
@@ -1,22 +1,12 @@
 # Stdlib Imports
 import uuid
 
+# Django imports
 from django.db import models
 from django.db.models import Index
+from django.contrib.auth import get_user_model
 
-class Company(models.Model):
-    """
-    Defines the schema for Assets_comapny table in the database.
-
-    Fields:
-        - id (int): the object unique uuid
-        - name (str): the name of country
-    """
-
-    id = models.UUIDField(
-        default=uuid.uuid4, unique=True, primary_key=True, editable=False
-    )
-    name = models.CharField(max_length=100)
+User = get_user_model()
 
 class Device(models.Model):
     """
@@ -28,31 +18,56 @@ class Device(models.Model):
         - company (Fk): Foreign Key to the company who issued the device
         - condition (str): Choice field for the condition of the device
     """
-    
-    id = models.UUIDField(
-        default=uuid.uuid4, unique=True, primary_key=True, editable=False
-    )
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    condition = models.CharField(max_length=100)
 
-class Employee(models.Model):
-    """
-    Defines the schema for Assets_comapny table in the database.
-
-    Fields:
-        - id (int): the object unique uuid
-        - name (str): the name of the employee
-        - company (Fk): Foreign Key to the company the employee works at
-        - issued_devices (m2m): many to many relationship to the Devices table to know how many devices have been issued to this employee
-    """
-    
-    id = models.UUIDField(
-        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+    CHOICES = (
+        ("Excellent", "Excellent"),
+        ("Good", "Good"),
+        ("Normal", "Normal"),
+        ("Bad", "Bad"),
     )
-    name = models.CharField(max_length=100)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    issued_devices = models.ManyToManyField(Device, through='Checkout')
+
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    company = models.ForeignKey(
+        'Company', on_delete=models.CASCADE,
+        related_name='device_company'
+    )
+
+    name = models.CharField(
+        max_length=100
+    )
+
+    condition = models.CharField(
+        max_length=100,
+        choices=CHOICES
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    is_checked_out = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Devices"
+        indexes = [
+            Index(fields=[
+                'name',
+                'id'
+            ]),
+        ]
+
+    def __str__(self) -> str:
+        return self.name + " owned by " + self.company.name + " in condition: " + self.condition
 
 class Checkout(models.Model):
     """
@@ -67,14 +82,118 @@ class Checkout(models.Model):
         - condition_out (str): Choice field for the condition of the device when it was checked out
         - condition_in (str): Choice field for the condition of the device when it was returned
     """
-    
-    id = models.UUIDField(
-        default=uuid.uuid4, unique=True, primary_key=True, editable=False
+
+    CHOICES = (
+        ("Excellent", "Excellent"),
+        ("Good", "Good"),
+        ("Normal", "Normal"),
+        ("Bad", "Bad"),
     )
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    checkout_date = models.DateTimeField(auto_now_add=True)
-    return_date = models.DateTimeField(null=True, blank=True)
-    condition_out = models.CharField(max_length=100)
-    condition_in = models.CharField(max_length=100, null=True, blank=True)
-    
+
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    company = models.ForeignKey(
+        'Company',
+        on_delete=models.CASCADE,
+    )
+
+    employee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    device = models.ForeignKey(
+        Device,
+        on_delete=models.CASCADE
+    )
+
+    checkout_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    return_date = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    condition_out = models.CharField(
+        max_length=100,
+        choices=CHOICES
+    )
+
+    condition_in = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True,
+        choices=CHOICES
+    )
+
+    class Meta:
+        verbose_name_plural = "Checkouts"
+        indexes = [
+            Index(fields=[
+                'id'
+            ]),
+        ]
+
+    def __str__(self) -> str:
+        return self.device + " checked out by " + self.employee + " on " + self.checkout_date + " and returned on " + self.return_date + " in condition: " + self.condition_in
+
+class Company(models.Model):
+    """
+    Defines the schema for Assets_comapny table in the database.
+
+    Fields:
+        - id (int): the object unique uuid
+        - name (str): the name of country
+    """
+
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+
+    name = models.CharField(
+        max_length=100
+    )
+
+    employees = models.ManyToManyField(
+        User,
+        related_name='company_employees',
+        editable=False
+    )
+
+    devices = models.ManyToManyField(
+        Device,
+        related_name='company_devices',
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
+
+    class Meta:
+        verbose_name_plural = "Companies"
+        indexes = [
+            Index(fields=[
+                'name',
+                'id'
+            ]),
+        ]
+
+    def __str__(self) -> str:
+        return self.name + " with total employees: " + str(self.employees.count()) + " and total devices: " + str(self.devices.count())
